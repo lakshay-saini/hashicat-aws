3dc0b6165ff5964462e129ec91944e212ce2f459
lakshay.laky@gmail.com <root@p-ciycymzya4bz-workstation.c.instruqt-prod.internal>
2020-07-02 11:40:53 +0000

    Updated my code.

--- main.tf
+++ main.tf
@@ -115,11 +115,20 @@ data aws_ami "ubuntu" {
 resource "aws_eip" "hashicat" {
   instance = aws_instance.hashicat.id
   vpc      = true
+
+  tags = {
+    Billable = "true"
+    Department = "devops"
+  }
 }
 
 resource "aws_eip_association" "hashicat" {
   instance_id   = aws_instance.hashicat.id
   allocation_id = aws_eip.hashicat.id
+  tags = {
+    Billable = "true"
+    Department = "devops"
+  }
 }
 
 resource aws_instance "hashicat" {
